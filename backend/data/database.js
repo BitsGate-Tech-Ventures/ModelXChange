88be55cfe281099ece80a4d65a2fa46cfa5153a6
Ayush Srivastava <105501429+AyushOJOD@users.noreply.github.com>
2024-06-27 02:51:24 +0530

    Revert "feat: implemented authentication"

--- backend/data/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-export async function connectDB() {
-  await mongoose.connect(process.env.MONGO_URL, {
-    dbName: process.env.DB_NAME,
-  });
-
-  console.log("Connected to the database.");
-}
