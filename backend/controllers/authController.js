e24f7b2927a76bb7b75ba987d5fe91f301ba42e5
Ayush <rishiustaad@gmail.com>
2024-06-27 02:48:27 +0530

    feat: implemented authentication

---
+++ backend/controllers/authController.js
@@ -0,0 +1,84 @@
+import { User } from "../models/userModel.js";
+import bcrypt from "bcryptjs";
+import { sendCookie } from "../utils/features.js";
+
+export const signup = async (req, res) => {
+  try {
+    const { email, password, confirmPassword, username, phoneNumber, role } =
+      req.body;
+
+    // Email validation using regular expression
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+    if (!emailRegex.test(email)) {
+      res.json({
+        success: false,
+        message: "Invalid email",
+      });
+    }
+
+    let user = await User.findOne({ phoneNumber, email });
+
+    if (user) {
+      res.json({
+        success: false,
+        message: "User already exists",
+      });
+    }
+
+    if (password !== confirmPassword) {
+      res.json({
+        success: false,
+        message: "Passwords do not match",
+      });
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 12);
+
+    user = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+      role,
+      phoneNumber,
+    });
+
+    sendCookie(user, res, "User created successfully");
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
+
+export const loginUsingEmailPassword = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email }).select("+password");
+
+    if (!user) {
+      res.json({
+        success: false,
+        message: "User does not exist",
+      });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      res.json({
+        success: false,
+        message: "Invalid credentials",
+      });
+    }
+
+    sendCookie(user, res, "Logged in successfully");
+  } catch (error) {
+    res.json({
+      success: false,
+      message: error.message,
+    });
+  }
+};
